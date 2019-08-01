d7cf401229df6512850f5618b8c3e31c3a06e8db
Theodore Si <sjyzhxw@gmail.com>
2019-08-02 00:54:46 +0800

    do 283

--- 283_move_zeros/move_zeros.py
+++ 283_move_zeros/move_zeros.py
@@ -3,20 +3,24 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-
+        We need a p1 to find the next zero, and a p2 to find the next non-zero
         """
         n = len(nums)
         if n <= 1:
             return None
         
+        # only 2 or more numbers are meaningful
+        
         p1, p2 = 0, 1
         while True:
             while p1 <= n-1 and nums[p1] != 0:
+                # Find the next zero
                 p1 += 1
-            if p2 == p1:
-            while p2 <= n-1 and nums[p2] == 0:
+            while p2 < p1 or (p2 <= n-1 and nums[p2] == 0):
+                # Find the next non-zero
                 p2 += 1
             if p2 == n:
                 break
-            nums[p1], nums[p2] = nums[p2], nums[p1]
-            p1 += 1
+            else:
+                nums[p1], nums[p2] = nums[p2], nums[p1]
+                p1 += 1
