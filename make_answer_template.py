3213a4dfa280fe50d80e32883145ef2f829b76a2
Theo Si <theosi@outlook.com>
2019-08-03 10:30:32 +0800

    create python script to generate answer template

---
+++ make_answer_template.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+
+import sys, os
+
+def main(q_no, q_title, method_signature):
+    answer_dir = '{}_{}'.format(q_no, q_title)
+    answer_file = '{}.py'.format(q_title)
+    os.mkdir(answer_dir)
+
+    with open(os.path.join(answer_dir, answer_file), 'w') as f:
+        content = """
+class Solution:
+    def {}:
+        pass
+        """.format(method_signature)
+        f.write(content)
+
+
+    with open(os.path.join(answer_dir, 'test_{}'.format(answer_file)), 'w') as f:
+        content = """
+import pytest
+from {} import Solution
+
+test_data_set = [
+]
+
+@pytest.mark.parametrize("", test_data_set)
+def test_solution():
+    solution = Solution()
+        """.format(q_title)
+        f.write(content)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: {} question_title method_signature'.format(sys.argv[0]))
+
+    original_title = sys.argv[1]
+    method_signature = sys.argv[2]
+
+    original_title_array = original_title.split('. ')
+
+    q_no = original_title_array[0]
+    q_title = original_title_array[1].lower().replace(' ', '_')
+    main(q_no, q_title, method_signature)
