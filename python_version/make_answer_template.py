661e630aa0613bd7898fbcf6a600d27e4e6e6d02
Theodore Si <theosi@outlook.com>
2021-04-25 12:02:44 +0800

    Do 485

--- python_version/make_answer_template.py
+++ python_version/make_answer_template.py
@@ -1,33 +1,44 @@
-#!/bin/env python
-
 import sys, os
+from enum import Enum, auto
 
-def main(q_no, q_title, method_signature):
+
+class DataStructure(Enum):
+    List = auto()
+    LinkedList = auto()
+
+def main(q_no, q_title, method_signature, data_structures=None):
+    if data_structures == None:
+        data_structures = []
     answer_dir = '{}_{}'.format(q_no, q_title)
     answer_file = '{}.py'.format(q_title)
-    os.mkdir(answer_dir)
+    try:
+        os.mkdir(answer_dir)
+    except FileExistsError:
+        pass
+    data_structure_to_imports = {
+        DataStructure.List: 'from typing import List',
+        DataStructure.LinkedList: 'from singly_linked_list import ListNode, make_linked_list, print_linked_list, linked_list_as_list'
+    }
+    data_structure_imports = '\n'.join([data_structure_to_imports[ds] for ds in data_structures])
 
     with open(os.path.join(answer_dir, answer_file), 'w') as f:
-        content = """#!/bin/env python
+        content = f"""import pytest
+{data_structure_imports}
+
+
 class Solution:
-    def {}:
+    def {method_signature}:
         pass
-        """.format(method_signature)
-        f.write(content)
 
-
-    with open(os.path.join(answer_dir, 'test_{}'.format(answer_file)), 'w') as f:
-        content = """#!/bin/env python
-import pytest
-from {} import Solution
 
 test_data_set = [
 ]
 
-@pytest.mark.parametrize("", test_data_set)
+
+@pytest.mark.parametrize('', test_data_set)
 def test_solution():
     solution = Solution()
-        """.format(q_title)
+        """.format(method_signature)
         f.write(content)
 
 
@@ -42,4 +53,4 @@ if __name__ == '__main__':
 
     q_no = original_title_array[0]
     q_title = original_title_array[1].lower().replace(' ', '_')
-    main(q_no, q_title, method_signature)
+    main(q_no, q_title, method_signature, [DataStructure.List, DataStructure.LinkedList])
